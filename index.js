08db1ba94d3f75c0b22b4a2693a0bb14e2a82504
tanvirIIUC <tanvirsohaniiuc@gmail.com>
2022-08-28 16:14:38 +0600

    aa

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const loadMeals = ()=>{
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+const loadMeals = (search)=>{
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+    fetch(url)
     .then(res =>res.json())
     .then(data =>displaymeal(data.meals))
 }
 
-loadMeals();
-
 const displaymeal = meals =>{
     const foodContainer = document.getElementById('food-container');
-
+    foodContainer.innerHTML = ``;
     meals.forEach(meal => {
-        console.log(meal);
+        // console.log(meal);
        const Div = document.createElement('div');
+       
        Div.classList.add('col');
 
        Div.innerHTML= `
          <div class="col">
-                  <div class="card">
+                  <div onclick="mealLoad(${meal.idMeal})" class="card">
                     <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                     <div class="card-body">
                       <h5 class="card-title">${meal.strMeal}</h5>
@@ -28,3 +28,35 @@ const displaymeal = meals =>{
        foodContainer.appendChild(Div);
     });
 } 
+ const searchFood = ()=>{
+  const searchFild= document.getElementById('search-fild');
+    const search = searchFild.value;
+    loadMeals(search);
+    searchFild.value = "";
+ }
+
+ const mealLoad = (meals) =>
+ {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meals}`
+  // console.log(url);
+  fetch(url)
+  .then(res => res.json())
+  .then(data => mealDetails(data.meals[0]))
+ }
+
+ const mealDetails = (details) =>
+ {
+     const mealContainer = document.getElementById('details');
+     mealContainer.innerHTML= ``;
+     const mealDiv = document.createElement('div');
+     mealDiv.innerHTML=`
+     <img src="${details.strMealThumb}" class="card-img-top" alt="...">
+     <div class="card-body">
+       <h5 class="card-title">${details.strMeal}</h5>
+       <p class="card-text">${details.strInstructions.slice(0,200)}</p>
+       <a href="#" class="btn btn-primary">Go somewhere</a>
+     </div>
+     `;
+     mealContainer.appendChild(mealDiv);
+     console.log(details)
+ }
